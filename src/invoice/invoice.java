7ede0b62a7a4590de2b837c3be2ddc4d30ab8fac
chintan <chintan791@gmail.com>
2016-03-07 13:21:44 -0500

    commit message

---
+++ src/invoice/invoice.java
@@ -0,0 +1,82 @@
+package invoice;
+
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class invoice {
+
+	public static void main(String[] args) {
+	
+		
+		NumberFormat nf= NumberFormat.getInstance();
+		nf.setMaximumFractionDigits(2);
+		nf.setMinimumFractionDigits(2);
+		nf.setRoundingMode(RoundingMode.HALF_UP);
+
+		Scanner sc = new Scanner (System.in);
+		
+		double value ;
+		int count = 0;
+		int i =0;
+		double rate;
+		double total = 0;
+		double tax = 0;
+		double grandTotal = 0;
+		
+		double [] d = new double [100];
+		
+		
+
+		
+		
+		System.out.println("Total rate ? : "  );
+		rate = sc.nextDouble();
+		
+		
+		
+		do
+		{
+			count++;
+			System.out.println("Price # " + count );
+			
+			d[i] = sc.nextDouble();
+			
+			value = d[i];
+			System.out.println (nf.format(d[i]));
+			
+			i++;
+		}
+		 while (value != 0.00);
+		
+		System.out.println("---------------------------------------------------");
+		
+		System.out.println("Receipt ");
+	
+	for (i=0; i <100 ; i++)
+	{
+		
+		System.out.println (nf.format(d[i]));
+		
+		total += d[i];
+		
+		if (d[i] == 0)
+			break;
+	}
+	
+	System.out.println(total + " subtotal");
+	
+	tax = total * rate;
+	
+	System.out.println(tax + " tax");
+	
+	grandTotal = tax + total;
+	
+	System.out.println(grandTotal + " grand total");
+	
+	
+	
+	
+	}
+
+}
